2ed67d4213e7a5077744a4e1d8f968a2d1e66d9b
dtfgenetics <green801028@gmail.com>
2026-07-13 05:57:58 -0500

    Mount GrowLens camera observation workflow

--- apps/growlens-web/src/main.tsx
+++ apps/growlens-web/src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AccountSyncWidget from './AccountSyncWidget';
 import App from './App';
+import CameraObservationWidget from './CameraObservationWidget';
 import ErrorBoundary from './ErrorBoundary';
 import './styles.css';
 import './account.css';
+import './camera.css';
 import './accessibility.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
+      <CameraObservationWidget />
       <AccountSyncWidget />
     </ErrorBoundary>
   </StrictMode>,
