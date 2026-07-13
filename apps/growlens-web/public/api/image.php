4e0d21d6b3cad5af95d1e2d86fbfd4abcb91f90c
dtfgenetics <green801028@gmail.com>
2026-07-13 15:05:38 -0500

    Coordinate private image reads with snapshots

--- apps/growlens-web/public/api/image.php
+++ apps/growlens-web/public/api/image.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/_images.php';
+require_once __DIR__ . '/_operations.php';
 
 growlens_require_method('GET');
+growlens_begin_storage_access();
 $context = growlens_current_session(true);
 $userId = (string)$context['user']['id'];
 $photoId = growlens_validate_photo_id($_GET['id'] ?? '');
