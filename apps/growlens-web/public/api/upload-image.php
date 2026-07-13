c37f345787c9e0fb6f7bf6cfb7d30f15ff97588b
dtfgenetics <green801028@gmail.com>
2026-07-13 15:05:23 -0500

    Coordinate image uploads with snapshots

--- apps/growlens-web/public/api/upload-image.php
+++ apps/growlens-web/public/api/upload-image.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/_images.php';
+require_once __DIR__ . '/_operations.php';
 
 growlens_require_method('POST');
 growlens_require_same_origin();
+growlens_begin_storage_access();
 $context = growlens_current_session(true);
 growlens_require_csrf($context);
 $userId = (string)$context['user']['id'];
