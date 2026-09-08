9b8bade5e4026766220ec202e5765398d77cd297
dtfgenetics <green801028@gmail.com>
2026-09-08 17:20:39 -0500

    Add world boss map and final boss key

---
+++ games/seed-man-platformer/src/systems/world-boss-map.mjs
@@ -0,0 +1,2 @@
+export const WORLD_BOSS_MAP=Object.freeze({'greenhouse-valley':'overgrown-guardian','forest-ruins':'ancient-dryad','desert-canyon':'scorchroot-titan','frozen-peaks':'frostbite-colossus','eco-city':'eco-sentinel'});
+export const FINAL_BOSS='blight-king';
